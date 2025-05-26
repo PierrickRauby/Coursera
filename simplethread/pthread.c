e0b1018abfa7fb45baab4ba5565c3dfb23d44274
PierrickRauby <pierrick.rauby@gadz.org>
2025-05-26 12:29:13 -0400

    updated code, need to try on the pi

--- simplethread/pthread.c
+++ simplethread/pthread.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sched.h>
+#include <syslog.h>
 
-#define NUM_THREADS 12
+#define NUM_THREADS 1
 
 typedef struct
 {
@@ -14,42 +15,42 @@ typedef struct
 // POSIX thread declarations and scheduling attributes
 //
 pthread_t threads[NUM_THREADS];
-threadParams_t threadParams[NUM_THREADS];
 
 
-void *counterThread(void *threadp)
+void *threadFunc(void *threadp)
 {
-    int sum=0, i;
-    threadParams_t *threadParams = (threadParams_t *)threadp;
-
-    for(i=1; i < (threadParams->threadIdx)+1; i++)
-        sum=sum+i;
- 
-    printf("Thread idx=%d, sum[0...%d]=%d\n", 
-           threadParams->threadIdx,
-           threadParams->threadIdx, sum);
+    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT:1] Hello World from Thread!");
+    return NULL;
 }
 
 
 int main (int argc, char *argv[])
 {
-   int rc;
    int i;
+
+   // open log
+   openlog("pthread", LOG_PID | LOG_CONS, LOG_USER);
+
+   // Need to add uname -r info
+
+   syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT:1] Hello World from Main!");
 
    for(i=0; i < NUM_THREADS; i++)
    {
-       threadParams[i].threadIdx=i;
 
        pthread_create(&threads[i],   // pointer to thread descriptor
                       (void *)0,     // use default attributes
-                      counterThread, // thread function entry point
-                      (void *)&(threadParams[i]) // parameters to pass in
+                      threadFunc, // thread function entry point
+                        NULL
+                    //   (void *)&(threadParams[i]) // parameters to pass in
                      );
 
    }
 
    for(i=0;i<NUM_THREADS;i++)
        pthread_join(threads[i], NULL);
+
+   closelog();
 
    printf("TEST COMPLETE\n");
 }
